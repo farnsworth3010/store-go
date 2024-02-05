38b72035d88f9f9945f783f5a6461c219b140e50
farnsworth <farnsworth3010@proton.me>
2024-02-05 21:38:30 +0300

    fix: infinite registration

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE users
 (
     id            serial primary key,
     name          varchar(255) not null,
-    username      varchar(255) not null,
+    username      varchar(255) not null unique,
     password_hash varchar(255) not null
 );
